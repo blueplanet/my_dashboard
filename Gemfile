ae8edbae55fe03f8a30ba6ee9aa9298641f06eef
blueplanet <erguolinge@gmail.com>
2015-12-23 19:59:26 +0900

    init

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'dashing'
+
+gem 'togglv8'
+gem 'awesome_print'
+
+group :development do
+  gem 'pry'
+end
---
+++ jobs/toggl.rb
@@ -0,0 +1,17 @@
+require 'togglv8'
+
+api = TogglV8::API.new(ENV['TOGGL_API_TOKEN'])
+
+SCHEDULER.every '5s' do
+  begin
+    current_time_entry = api.get_current_time_entry
+    description = current_time_entry['description'] || '説明なし'
+    project_name = 'プロジェクト名称'
+
+    unless current_time_entry['pid'].nil?
+      project_name = api.get_project(current_time_entry['pid'])['name']
+    end
+
+    send_event('current_task', title: project_name, text: description)
+  end
+end
